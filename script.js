dc3cd688752d4c81bbe0f3e1adfa3dfa75523fbe
GoravRAj <goravtriton@gmail.com>
2026-03-08 16:12:36 +0530

    Created functionality for displaying input
    Now user can use keyboard or ui for input

--- script.js
+++ script.js
@@ -3,6 +3,29 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => b === 0 ? "Nope!" : a / b;
 
+const operators = ["+", "-", "/", "*", "%"];
+
+const userInput = document.querySelector("#user-input");
+const btnContainer = document.querySelector(".btns-container");
 
 
-console.log(divide(1,0));
+
+
+function updateDisplay(val) {
+    if(operators.includes(val) || Number(val)) userInput.value += val;
+}
+
+
+
+btnContainer.addEventListener("click", (e) => {
+    updateDisplay(e.target.textContent); 
+});
+
+
+
+
+window.addEventListener("keydown", (e) => {
+    updateDisplay(e.key);
+});
+
+
