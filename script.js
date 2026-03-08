09f4b8cda694fa93e5c8a68a3f9e4b04d9eca834
GoravRAj <goravtriton@gmail.com>
2026-03-08 10:10:01 +0530

    Created math operation functions

---
+++ script.js
@@ -0,0 +1,8 @@
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => b === 0 ? "Nope!" : a / b;
+
+
+
+console.log(divide(1,0));
