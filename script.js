6152d8e5465d851d571a42729993e9059b1fbc1b
GoravRAj <goravtriton@gmail.com>
2026-03-20 14:16:56 +0530

    Added functionality for "AC" and "Clear" buttons
    Now the calculator is compelete

--- script.js
+++ script.js
@@ -34,18 +34,21 @@ function handleInput(val) {
             secondNum += val;
         }
 
-    } else if (val in operations) {
+    }
+
+    // Operations
+    else if (val in operations) {
         if (firstNum && currentOp && secondNum) {
             firstNum = String(operate(currentOp, firstNum, secondNum));
             secondNum = "";
         }
         if (firstNum) {
             currentOp = val;
-            shouldReset = false; 
+            shouldReset = false;
         }
     }
 
-
+    // Enter & Equal
     else if (val === "=" || val === "Enter") {
         if (firstNum && currentOp && secondNum) {
             firstNum = String(operate(currentOp, firstNum, secondNum));
@@ -53,6 +56,22 @@ function handleInput(val) {
             currentOp = null;
             shouldReset = true;
         }
+    }
+
+    //  Clear & AC
+    else if (val === "Clear" || val === "Backspace") {
+        if (secondNum) {
+            secondNum = secondNum.slice(0, -1);
+        } else if (currentOp) {
+            currentOp = null; // Delete the operator if no second number exists
+        } else {
+            firstNum = firstNum.slice(0, -1);
+        }
+    } else if (val === "AC") {
+        firstNum = "";
+        secondNum = "";
+        currentOp = null;
+        shouldReset = false;
     }
 
     // Update Display
@@ -76,7 +95,7 @@ btnContainer.addEventListener("click", (e) => {
 });
 
 window.addEventListener("keydown", (e) => {
-    // Map 'Enter' to '=' and prevent default browser behavior
+    // Map 'Enter' to '=' 
     if (e.key === "Enter") e.preventDefault();
     handleInput(e.key);
 });
